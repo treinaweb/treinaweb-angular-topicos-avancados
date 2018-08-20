c7596ecd91ad627ff6088ba2e30c678d09f5224d
Felipe Hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2018-08-20 15:55:51 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
       {
         path: ':nome',
         component: CoursesListComponent,
-        canActivate: [ BloqueadorGuard ]
+        canDeactivate: [ BloqueadorGuard ]
       }
     ]
   }
--- src/app/guards/bloqueador.guard.ts
+++ src/app/guards/bloqueador.guard.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanDeactivate } from '@angular/router';
 import { Observable } from 'rxjs';
+import { CoursesListComponent } from '../courses-list/courses-list.component';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BloqueadorGuard implements CanActivate {
+export class BloqueadorGuard implements CanActivate, CanDeactivate<CoursesListComponent> {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot):  boolean {
 
       console.log(next, state)
+    return false;
+  }
+
+  canDeactivate(
+    component: CoursesListComponent,
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean{
+    console.log(component, next, state);
     return false;
   }
 }
