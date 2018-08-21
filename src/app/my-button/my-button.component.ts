2262417c6d6d3024388aad7ae81b0ddd984e85eb
Felipe Hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2018-08-21 17:29:52 -0300

    initial commit

--- src/app/my-button/my-button.component.ts
+++ src/app/my-button/my-button.component.ts
@@ -23,11 +23,27 @@ import {
       })),
       transition('inactive => active', animate('100ms ease-in')),
       transition('active => inactive', animate('100ms ease-out'))
+    ]),
+    trigger('listState', [
+      state('in', style({transform: 'translateX()0', opacity: 1})),
+      transition('void => *', [
+        style({
+          transform: 'translateX(-50%)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(30%)',
+          opacity: 0
+        }))
+      ])
     ])
   ]
 })
 export class MyButtonComponent implements OnInit {
   myState = 'active';
+  myList = [];
 
   constructor() { }
 
@@ -35,7 +51,16 @@ export class MyButtonComponent implements OnInit {
   }
 
   toggleState(){
+    this.insertItem();
     this.myState = this.myState === 'active' ? 'inactive' : 'active';
+  }
+
+  insertItem(){
+    this.myList.push(this.myList.length);
+  }
+
+  removeItem(index){
+    this.myList.splice(index, 1);
   }
 
 }
