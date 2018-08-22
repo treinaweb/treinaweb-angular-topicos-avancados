08f3c192c0a4072b65a1c5c3a4bb4fa36c3821ff
Felipe Hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2018-08-22 16:45:15 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import localeBR from '@angular/common/locales/pt';
 
 import { AppComponent } from './app.component';
 import { CoursesModule } from './courses/courses.module';
 import { AppRoutingModule } from './app-routing.module';
 import { MyLibraryModule } from 'my-library';
 import { MyButtonComponent } from './my-button/my-button.component';
+
+
+registerLocaleData(localeBR);
 
 @NgModule({
   declarations: [
