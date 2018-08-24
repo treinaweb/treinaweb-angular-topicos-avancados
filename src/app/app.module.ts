6a91b5124f2b62c2ac2c5ced5f1ac432a1cc94ce
Felipe Hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2018-08-24 20:25:18 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { RollOnScrollDirective } from './directives/roll-on-scroll.directive';
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/pokedex/components/list-item/list-item.component.ts
+++ src/app/modules/pokedex/components/list-item/list-item.component.ts
@@ -1,9 +1,32 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.css']
+  styleUrls: ['./list-item.component.css'],
+
+  animations: [
+    trigger('listItemState', [
+      state('in', style({transform: 'translateX(0)', opacity: 1})),
+      transition(':enter', [
+        style({
+          transform: 'translateX(-50%)'
+        }),
+        animate(300)
+      ]),
+      transition(':leave', style({
+        transform: 'translateX(30%)',
+        opacity: 0
+      }))
+    ])
+  ]
 })
 export class ListItemComponent implements OnInit {
   @Input() pokemon: any;
